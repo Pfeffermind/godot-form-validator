7975ceb80c1b533ac53921e53b211dcf9690b21e
niko-pfeffi <nikolas.joop@pfeffermind.de>
2026-05-07 14:10:47 +0200

    fix paths

--- addons/godot-form-validator/plugin.gd
+++ addons/godot-form-validator/plugin.gd
@@ -3,7 +3,7 @@ extends EditorPlugin
 
 
 func _enter_tree() -> void:
-	add_autoload_singleton("Validation", "res://addons/godot-form-validator/validation.gd")
+	add_autoload_singleton("Validation", "res://addons/godot-form-validator/addons/godot-form-validator/validation.gd")
 	add_custom_type("FormValidator", "Control", preload("form_validator.gd"), preload("editor_icon.png"))
 	add_custom_type("ControlValidator", "Node", preload("control_validator.gd"), preload("editor_icon.png"))
 
